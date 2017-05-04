bf8af1f6b700191f6c369a70aa32d5fde767574c
David Amirtahmasebi <Davidamirth@gmail.com>
2017-05-04 20:30:50 +0200

    La til grids

--- PHP/lonningsdag.php
+++ PHP/lonningsdag.php
@@ -9,7 +9,7 @@
 <meta name="author" content="">
 
     <title>Campus Vulkan</title>
-    <link rel="stylesheet" type="text/css" href="/CSS/campusVulkan.css">
+    <link rel="stylesheet" type="text/css" href="/CSS/lonningsdag.css">
     
 </head>
 
@@ -25,6 +25,40 @@
     <p class="mainText">UTBETALING!</p>
         </div>
         
+        
+        <div id="space"></div>
+    <div class ="grid">
+        <div class="containers">
+            <div id="Overskrift">Seksjon 1.10.32 av "de Finibus Bonorum et Malorum".</div>
+            <div id="Undertekst">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"</div>
+            <div id="Brodtekst">Standardavsnittet for Lorem Ipsum, brukt siden 1500-tallet
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit blad isp ciddu dono sekimi fetsois</div>
+            </div>
+        <div class="containers"><img src="http://placehold.it/500x300" id="Bilder"</a></div>
+        <div class="containers">
+        <div id="Overskrift">Seksjon 1.10.32 av "de Finibus Bonorum et Malorum".</div>
+            <div id="Undertekst">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"</div>
+            <div id="Brodtekst">Standardavsnittet for Lorem Ipsum, brukt siden 1500-tallet
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit blad isp ciddu dono sekimi fetsois</div>
+            </div>
+        <div class="containers"><img src="http://placehold.it/500x300" id="Bilder"</a></div>
+        
+        <div class="containers">
+            <div id="Overskrift">Seksjon 1.10.32 av "de Finibus Bonorum et Malorum".</div>
+            <div id="Undertekst">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"</div>
+            <div id="Brodtekst">Standardavsnittet for Lorem Ipsum, brukt siden 1500-tallet
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit blad isp ciddu dono sekimi fetsois</div>
+            </div>
+        <div class="containers"><img src="http://placehold.it/500x300" id="Bilder"</a></div>
+     
+        </div>
+
+            
+                
+   
     <?php require 'footer.php'; ?>
 
     </body>
